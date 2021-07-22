c6f82a22b2529fad18d9a2862558d3d8ccf0dc1c
Thiago Carvalho de Onofre Gonçalves <thiago.cgoncalves@al.infnet.edu.br>
2021-07-22 23:10:23 +0100

    map address tooltip ok

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ import initAnimacaoScroll from "./modules/scroll-animacao.js"
 import initAccordion from "./modules/accordion.js"
 import initTabNav from "./modules/tabnav.js"
 import initModal from "./modules/modal.js"
+import initTooltip from "./modules/tooltip.js"
 
 // ---------- Execuções das funções importadas ----------
 initScrollSuave()
@@ -11,3 +12,4 @@ initAnimacaoScroll()
 initAccordion()
 initTabNav()
 initModal()
+initTooltip()
