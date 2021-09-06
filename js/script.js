1684396c3bf4398f7e7f4167ec67323748417ed7
Thiago Carvalho de Onofre Gonçalves <thiago.cgoncalves@al.infnet.edu.br>
2021-09-06 19:47:42 +0100

    scroll suave transformado em classe

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 // ---------- Importações de módulos ----------
-import initScrollSuave from "./modules/scroll-suave.js"
+import ScrollSuave from "./modules/scroll-suave.js"
 import initAnimacaoScroll from "./modules/scroll-animacao.js"
 import initAccordion from "./modules/accordion.js"
 import initTabNav from "./modules/tabnav.js"
@@ -12,7 +12,9 @@ import initFetchAnimais from "./modules/fetch-animais.js"
 import initFetchBitcoin from "./modules/fetch-bitcoin.js"
 
 // ---------- Execuções das funções importadas ----------
-initScrollSuave()
+const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]')
+scrollSuave.init()
+
 initAnimacaoScroll()
 initAccordion()
 initTabNav()
